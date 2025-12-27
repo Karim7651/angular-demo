7da425607c2b4dec810e1addb66d36cacaa6e09c
Karim Shaaban <karimatef785@gmail.com>
2025-12-27 02:30:04 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Products } from './products/products';
+import { Contact } from './contact/contact';
+import { Error } from './error/error';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'products', component: Products },
+  { path: 'contact', component: Contact },
+  { path: '**', component: Error }
+];
